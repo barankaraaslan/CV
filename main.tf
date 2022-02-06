9a0583fb379d0e647945ecf673770a632df54d58
Baran Karaaslan <barankaraaslan_06@hotmail.com>
2022-02-06 05:19:08 +0300

    cv-as-service file triggers are reduced

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ terraform {
 }
 
 provider "aws" {
+  profile = "default"
   alias  = "us_east_1"
   region = "us-east-1"
 }
@@ -22,11 +23,64 @@ resource "aws_ecrpublic_repository" "cv-builder" {
   repository_name = "cv-builder"
 }
 
-resource "aws_ecs_cluster" "foo" {
-  name = "white-hart"
+resource "aws_ecs_cluster" "cv-as-service" {
+  name = "cv-as-service-iaac"
+}
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
+data "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+}
+
+resource "aws_ecs_task_definition" "cv-as-service" {
+  family = "cv-as-service"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn = data.aws_iam_role.ecsTaskExecutionRole.arn
+  task_role_arn = data.aws_iam_role.ecsTaskExecutionRole.arn
+  container_definitions = jsonencode([
+    {
+      name      = "cv-as-service"
+      image     = "public.ecr.aws/g6k1i6y8/cv-as-service:latest"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+            "awslogs-group": "/ecs/manuel-task-cv-as-service",
+            "awslogs-region": "us-east-1",
+            "awslogs-stream-prefix": "ecs"
+        }
+      }
+    }  
+  ])
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+}
+
+
+data "aws_subnet_ids" "default" {
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_ecs_service" "cv-as-service" {
+  name            = "cv-as-service"
+  cluster         = aws_ecs_cluster.cv-as-service.id
+  task_definition = aws_ecs_task_definition.cv-as-service.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets = data.aws_subnet_ids.default.ids
+    assign_public_ip = "true"
   }
 }
